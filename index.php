ef8d41aebb3f13f1dfe2160eab0e83df98d30c04
Darmawan Sidiq <dmwsdq@users.noreply.github.com>
2014-06-28 09:34:38 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="main.php" method="post">
+<!-- custom these to suit your needs -->
+<input type="radio" name="category" value="Kecamatan_di_Kabupaten_Rembang">Kecamatan di Kabupaten Rembang<br>
+<input type="radio" name="category" value="Kecamatan_di_Kabupaten_Blora">Kecamatan di Kabupaten Blora<br>
+<input type="submit">
+</form>
+
+</body>
+</html>
