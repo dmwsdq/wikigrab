29d140a40f427e67813a8f1a5303e8a46e8b4dee
Darmawan Sidiq <dmwsdq@users.noreply.github.com>
2014-08-02 16:43:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <!-- custom these to suit your needs -->
 <input type="radio" name="category" value="Kecamatan_di_Kabupaten_Rembang">Kecamatan di Kabupaten Rembang<br>
 <input type="radio" name="category" value="Kecamatan_di_Kabupaten_Blora">Kecamatan di Kabupaten Blora<br>
+Artikel Awal: <input type="text" name="start"><br>
 <input type="submit">
 </form>
 
